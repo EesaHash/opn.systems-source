bac7a99918c3a4885bd3929a1ab834bc3852b8bf
Eesa Hashmi <eesa@Eesas-MacBook-Pro.local>
2023-05-31 16:36:49 +1000

    added test database config and registration controller

--- back-end/index.js
+++ back-end/index.js
@@ -11,11 +11,7 @@ require("./controllers/accountController/connectDatabase").connectDatabase();
 
 // ACCOUNT APIs CONTROLLERS
 app.use("/api/login", require("./controllers/accountController/loginController"));
-<<<<<<< HEAD
-app.use("/api/login", require("./controllers/accountController/registrationController"));
-=======
-app.use("/api/authenticatelogin", require("./controllers/accountController/AuthenticateLoginController"));
->>>>>>> e12d20c548c55c5ef2f99f8a412a08bb4fc45d3b
+app.use("/api/signup", require("./controllers/accountController/registrationController"));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
