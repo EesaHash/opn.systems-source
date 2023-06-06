c5fd9df5e0880f5ac4892eee4919a046e234038d
OmkarBodas <omkarsbodas@gmail.com>
2023-06-06 12:52:27 +1000

    added changes

--- front-end/src/dashboard/component/Dashboard.js
+++ front-end/src/dashboard/component/Dashboard.js
@@ -1,26 +1,53 @@
 import React, {useEffect, useState} from "react";
 import "../style/dashboard.css";
 import { getUserID } from "../../App";
+
+
+
+
+function SignOut(setUserID) {
+  setUserID("none");
+}
+
+function varLink(userID,setUserID) {
+    if (userID === "none") {
+        return (
+            <>
+            <a href="/SignUp" className="signup">Sign up</a>
+            <a href="/SignIn" className="login">Login</a>
+            </>
+        );
+    }
+    else {
+        return <button onClick={(SignOut(setUserID))} className="signout">Sign Out
+        </button>
+    }
+
+}
+
 export const Dashboard = _ => {
      const [userID, setUserID] = useState();
-
      useEffect(() => {
          try{
-             getUserID().then(res => setUserID(res));
+             getUserID().then(res => setUserID(res)); 
          }catch(error){
              console.log(error);
          }
      }, []);
 
-    // if(userID === "none") return window.location.href = "/signin";
+    //  if (userID === "none") {
+    //     return window.location.href = "/SignIn"
+    //  }
+
+
+    
   return (
     <div>
       <header>
         <a href="https://www.instagram.com/" className="circle"></a>
         <a href="https://www.instagram.com/" className="text">Features</a>
         <a href="https://www.instagram.com/" className="text">About OPN</a>
-        <a href="/SignUp" className="signup">Sign up</a>
-        <a href="/SignIn" className="login">Login</a>
+        {varLink(userID, setUserID)}
       </header>
       <div className="center_head">SOPs Optimized</div>
       <div className="mission">AI generated. Streamlined SOPs. Continuous improvement.<br />Your operations made Easy!</div>
