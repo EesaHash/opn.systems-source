b6fcb70209c4a53e4f2af77387426cd7fa4cdf31
trishmanjunath <trishati02@gmail.com>
2023-06-08 09:29:08 +1000

    dashboard

--- front-end/src/homepage/component/dash.js
+++
@@ -1,4 +0,0 @@
-import React, {useState} from "react";
-import "../style/dashboard.css";
-import { getUserID } from "../../App";
-
--- front-end/src/homepage/component/homepage.js
+++ front-end/src/homepage/component/homepage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import "../style/dashboard.css";
+import "../style/homepage.css";
 import { getUserID } from "../../App";
 
 
