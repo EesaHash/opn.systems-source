2e7dae12383973db58e952ac41f167a598490c82
jonathanertanto <jonathanertanto@gmail.com>
2023-06-08 10:55:38 +1000

    forget password api connection on front-end

--- front-end/src/account/components/ForgotPassword.js
+++ front-end/src/account/components/ForgotPassword.js
@@ -4,38 +4,35 @@ import { getUserID } from "../../App";
 export const ForgotPassword = _ => {
     const [userID, setUserID] = useState("none");
     getUserID().then(res => setUserID(res));
+    
+    const handleKeypress = e => {
+        if(e.key === "Enter"){
+            forgetPassword();
+        }
+    };
 
     if(userID !== "none") return window.location.href = "/";
     return(
-    <div className="page"> 
-        <div className="cover" style={{height: "35%", width: "100%", overflow: "scroll"}}>
-        
-                    <div className="content">
-                <div className ="user-authentication">
-    <div>    
-            <h1>Forgot Password</h1>
-    
-            <div className="user-authentication-input" >
-            <label>Recovery Email Address</label>
-            <input type="text" placeholder="johndoe@gmail.com" id="email"></input>
-            </div>
-            <div>
-             <button onClick={backToLogin}>Send Email</button>
-             
-            </div>
-    </div>
-                </div>
-            
-        </div> 
-    </div> 
-    </div>
-   
+        <div className="page"> 
+            <div className="cover" style={{height: "35%", width: "100%", overflow: "scroll"}}>
+                <div className="content">
+                    <div className ="user-authentication">
+                        <div>
+                            <h1>Forgot Password</h1>
+                            <div className="user-authentication-input" >
+                                <label>Recovery Email Address</label>
+                                <input type="text" placeholder="johndoe@gmail.com" id="email" onKeyPress={handleKeypress} />
+                            </div>
+                            <div>
+                                <button onClick={forgetPassword}>Send Email</button>
+                            </div>
+                        </div>
+                    </div> 
+                </div> 
+            </div> 
+        </div>
     );
 };
-
-function backToLogin() {
-    return window.location.href = "/SignIn";
-}
 
 const forgetPassword = _ => {
     try{
@@ -52,6 +49,8 @@ const forgetPassword = _ => {
             .then((res) => {return res.json(); })
             .then((data) => {
                 alert(data.message);
+                if(data.status)
+                    return window.location.href = "/SignIn";
             });
     }catch(error){
         console.log(error);
