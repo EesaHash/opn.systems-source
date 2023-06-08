b0073e7125e04272ac2d80d11d32d5c1ef31191f
trishmanjunath <trishati02@gmail.com>
2023-06-08 14:55:39 +1000

    dash logo

--- front-end/src/homepage/component/dashboard.js
+++ front-end/src/homepage/component/dashboard.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import "../style/homepage.css";
+import "../style/dashboard.css";
 import { getUserID, logOut } from "../../App";
 
 export const Dashboard = _ => {
@@ -13,17 +14,9 @@ export const Dashboard = _ => {
      }, []);
     if(userID === "none") return window.location.href = "/";
     return(
-       <html>
-        <head>
-            <meta charSet="UTF-8"></meta>
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-
-            <link rel="stylesheet" href="dashboard.css"></link>
-        </head>
         <body>
-            <nav className="sidebar"></nav>
-        </body>
-       </html>
+            <div className="logo1">
+            </div>            
+        </body> 
     );
 };
