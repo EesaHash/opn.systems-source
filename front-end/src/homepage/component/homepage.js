02d0d9b20935494f4787ec9a62c21bd38022d7fd
jonathanertanto <jonathanertanto@gmail.com>
2023-06-08 10:25:22 +1000

    homepage

--- front-end/src/homepage/component/homepage.js
+++ front-end/src/homepage/component/homepage.js
@@ -2,50 +2,13 @@ import React, { useState } from "react";
 import "../style/homepage.css";
 import { getUserID } from "../../App";
 
-function SignOut(setUserID) {
-  setUserID("none");
-}
-
-function varLink(userID,setUserID) {
-    if (userID === "none") {
-        return (
-            <>
-            <div className="buttons">
-            <a href="/SignUp" className="signup">Sign up</a>
-            <a href="/SignIn" className="login">Login</a>
-            </div>
-            </>
-        );
-    }
-    else {
-        return <button onClick={(SignOut(setUserID))} className="signout">Sign Out
-        </button>
-    }
-
-}
-
-export const Dashboard = _ => {
-     const [userID, setUserID] = useState();
-     useEffect(() => {
-         try{
-             getUserID().then(res => setUserID(res)); 
-         }catch(error){
-             console.log(error);
-         }
-     }, []);
-
-    //  if (userID === "none") {
-    //     return window.location.href = "/SignIn"
-    //  }
-
-//style={{overflow: "scroll"}} for scrolling
 export const Homepage = _ => {
   const [userID, setUserID] = useState("none");
   getUserID().then(res => setUserID(res));
 
   if(userID !== "none") return window.location.href = "/dashboard"
   return (
-    <div className = "page"> 
+    <div className = "page" style={{overflow: "scroll"}}>
         <div className="ricky">
           <div className="header">
            <div className="circle"></div>
