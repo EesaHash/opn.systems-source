b6f61014e779dccb5133602d49286d1027893355
jonathanertanto <jonathanertanto@gmail.com>
2023-06-09 10:56:27 +1000

    add header to user-authentication

--- front-end/src/header/components/Header.js
+++ front-end/src/header/components/Header.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { getUserID } from "../../App";
+import "../style/header.css";
 
-export const Header = _ => {
+export const Header = (props) => {
+    const [userID, setUserID] = useState();
+    useEffect(() => {
+        try {
+            getUserID().then(res => setUserID(res));
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
     return(
         <main>
             <header>
+                {props.page !== "none" && navbar(props.page)}
             </header>
             <div id="content" className="content">
                 <Outlet/>
             </div>
         </main>
+    );
+};
+
+const navbar = (page) => {
+    return(
+        <div className="menu-bar">
+            <ul>
+                {page === "signin" || page === "signup" || page === "forgetpass"}
+                <li id="logo" className="logo"><a href="/"><img src="./images/green_profile_logo.png" alt="logo"/>Opn.Systems</a></li>
+            </ul>
+        </div>
     );
 };
