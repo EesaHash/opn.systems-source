7c3c7393e9675275ac29ec7f5b9e590be9574169
jonathanertanto <jonathanertanto@gmail.com>
2023-06-16 15:51:58 +1000

    homepage

--- front-end/src/header/components/Header.js
+++ front-end/src/header/components/Header.js
@@ -1,26 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
-import { getUserID } from "../../App";
 import "../style/header.css";
 
 export const Header = (props) => {
-    const [userID, setUserID] = useState();
-    useEffect(() => {
-        try {
-            getUserID().then(res => setUserID(res));
-        } catch (error) {
-            console.log(error);
-        }
-    }, []);
 
     return(
         <main>
             <header>
                 {props.page !== "none" && navbar(props.page)}
             </header>
-            <div id="content" className="content">
-                <Outlet/>
-            </div>
+            <Outlet/>
         </main>
     );
 };
--- front-end/src/homepage/component/homepage.js
+++ front-end/src/homepage/component/homepage.js
@@ -3,30 +3,43 @@ import "../style/homepage.css";
 import { getUserID } from "../../App";
 
 export const Homepage = _ => {
-  const [userID, setUserID] = useState("none");
-  getUserID().then(res => setUserID(res));
+    const [userID, setUserID] = useState("none");
+    getUserID().then(res => setUserID(res));
 
-  if(userID !== "none") return window.location.href = "/dashboard"
-  return (
+    if(userID !== "none") return window.location.href = "/dashboard"
+    return (
     <div className = "page" style={{overflow: "scroll"}}>
         <div className="ricky">
-          <div className="header">
-           <div className="circle"></div>
-          <a href="https://www.instagram.com/" className="text">Features</a>
-          <a href="https://www.instagram.com/" className="text">About OPN</a>
-           <div className="buttons">
-            <a href="/SignUp" className="signup">Sign up</a>
-            <a href="/SignIn" className="login">Login</a>
-          </div>
-          </div>
-          <div className="center_head">SOPs Optimized</div>
-          <div className="mission">AI generated. Streamlined SOPs. Continuous improvement.<br />Your operations made Easy!</div>
-          <div className="page_1">
-          </div>
-          <div className="page_2"></div>
-          <div className="page_3"></div>
-          <div className="page_4"></div>
+            <div className="header">
+                <div className="circle"></div>
+                <a href="#features" className="text">Features</a>
+                <a href="#about" className="text">About OPN</a>
+                <div className="buttons">
+                    <a href="/SignUp" className="signup">Sign up</a>
+                    <a href="/SignIn" className="login">Login</a>
+                </div>
+            </div>
+            <div className="page_1">
+                <div className="center_head">SOPs Optimized</div>
+                <div className="mission">AI generated. Streamlined SOPs. Continuous improvement.<br />Your operations made Easy!</div>
+                <div className="free-trial">
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="username"/>
+                        <label for="floatingInput">Email Address</label>
+                    </div>
+                    <button>{`Start your free trial >`}</button>
+                </div>
+            </div>
+            <div className="page_2">
+
+            </div>
+            <div className="page_3">
+
+            </div>
+            <div className="page_4">
+
+            </div>
         </div>
     </div>
-  );
+    );
 };
