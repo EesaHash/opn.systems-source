2531e3eb698331aa6c5e7e4d0b62bb1d48ee7ff1
OmkarBodas <omkarsbodas@gmail.com>
2023-06-16 15:38:30 +1000

    dashboard2

--- front-end/src/dashboard/components/dashboard2.js
+++ front-end/src/dashboard/components/dashboard2.js
@@ -18,42 +18,37 @@ export const Dashboard2 = _ => {
     return (
       <section className="dashboard2">
       <div className="dashboard2-main">
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: '280px' , height: '100vh' }}>
-          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <svg className="sidebar">
-              <use xlinkHref="https://www.instagram.com/"></use>
-            </svg>
-            <span className="title" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
-              Opn. Systems
-            </span>
-          </a>
-          <hr/>
-          <ul className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
-              <a href="#" className="nav-link active" aria-current="page">
-                <svg className="bi pe-none me-2" width="16" height="16">
-                  <use xlinkHref="#home"></use>
-                </svg>
-                Dashboard
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <svg className="bi pe-none me-2" width="16" height="16">
-                  <use xlinkHref="#speedometer2"></use>
-                </svg>
-                Add Business
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="#">Business 1</a></li>
-                <li><a className="dropdown-item" href="#">Business 2</a></li>
-              </ul>
-            </li>
-          </ul>
-          <hr />
-        </div>
+        <div className="dash">
+            <a href="/" className="dash_list">
+              <div className="title">
+                  <ul>
+                      <li id="logo" className="logo"><a href="/dashboard"><img src="./images/green_profile_logo.png" alt="logo"/>Opn.Systems</a></li>
+                  </ul>
+              </div>
+              <hr/>
+            </a>
+            <hr/>
+            <ul className="sidebar nav-pills">
+              <li className="navigation-item">
+                <a href="#" className="navigation_link">
+                  <svg className="bi pe-none me-2" width="16" height="16">
+                   <path""/>
+                  </svg>
+                  Dashboard
+                </a>
+              </li>
+              <li className="business dropdown">
+                <a className="navigation_link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <svg className="bi pe-none me-2" width="16" height="16">
+                    <rect width="16" height="16" fill="blue" /> {/* Replace with your desired icon */}
+                  </svg>
+                  Add Business
+                </a>
+              </li>
+            </ul>
+            <hr />
+          </div>
       </div>
     </section>
-  
   );
 };
