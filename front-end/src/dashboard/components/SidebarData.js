2bd30a4a7d801da690975940d583919a5dc83ce2
jonathanertanto <jonathanertanto@gmail.com>
2023-06-19 17:03:01 +1000

    dashboard

--- front-end/src/dashboard/components/SidebarData.js
+++ front-end/src/dashboard/components/SidebarData.js
@@ -3,7 +3,6 @@ import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import WorkIcon from '@mui/icons-material/Work';
 
-
 export const SidebarData = [
     {
         title: "Dashboard",
