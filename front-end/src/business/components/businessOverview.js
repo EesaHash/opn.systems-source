23f74b71f5c491437d2046dd443eb1cabd72b7eb
trishmanjunath <trishati02@gmail.com>
2023-07-19 15:50:09 +1000

    business overview

--- front-end/src/business/components/businessOverview.js
+++ front-end/src/business/components/businessOverview.js
@@ -1,21 +1,38 @@
 import React from 'react';
 //import "../style/business.css";
 import "../style/businessOverview.css"
-
+import ChatIcon from '../svg/chatIcon';
+import PeopleIcon from '../svg/peopleIcon';
 
 
 export const businessOverview = (business) => {
     return(
         <div className="businesses-overview">
-                <div className="yourbusiness">
-                    <div className="businesstitle">
-                        <text>Business Description </text>  
-                    </div>
-                    <div className='desciption-input'>
-                        <input type ="text" placeholder='What is your business about?'></input>
-                    </div>
-                    <button className="editB">How to edit your files</button>
+        <div className="yourbusiness">
+            <div className="businesstitle">
+                <text>Welcome to Opn.Systems </text>
+                <img className="party" src="./images/partypopper.png" alt="party"/>
+                <p style={{fontSize:"14px"}}>Here's some tips to get you started</p>
+                <div style={{justifyContent:"center", alignItems:"center", display:"inline-flex", gap:"8px"}}>
+                <button className="howToButtons"><ChatIcon/> How to edit your files</button>
+                <button className="howToButtons"><ChatIcon/> How to connect other apps</button>
+                
                 </div>
+            
+                <div className="container">
+                    <div className="message-container">
+                        <div className="message">
+                        <div className="text"><PeopleIcon/> You’ve reached your business limit (1/1)</div>
+                        </div>
+                    </div>
+                    <div className="upgrade-container">
+                        <div className="upgrade-text">
+                        When you upgrade to Premium, you can create unlimited business, roles, and invite your team members.
+                        </div>
+                    </div>
+                    </div>
+                </div>
+        </div>
                 
 
                 <div className="updates">
---
+++ front-end/src/business/svg/peopleIcon.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="18"
+      fill="none"
+      viewBox="0 0 19 18"
+    >
+      <path
+        stroke="#111827"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M12.75 5.25a3 3 0 11-6 0 3 3 0 016 0zM9.75 10.5a5.25 5.25 0 00-5.25 5.25H15c0-2.9-2.35-5.25-5.25-5.25z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
