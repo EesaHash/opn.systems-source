c567c57ca38607a2b71f05b7fbec8d8eda8a8220
jonathanertanto <jonathanertanto@gmail.com>
2023-07-21 16:50:14 +1000

    sop frontend (unfinished)

--- front-end/src/table/components/List/ListItem.js
+++ front-end/src/table/components/List/ListItem.js
@@ -7,12 +7,69 @@ import { dashPattern, letterPattern, numberingPattern, stepPattern } from '../Pa
 export const ListItem = (props) => {
     const [itemClassName, setItemClassName] = useState("minimised");
     const [selectedStep, setSelectedStep] = useState(0);
+    const [list, setList] = useState([]);
 
     useEffect(() => {
         setItemClassName("minimised");
+        let indent = false;
+        let heading = "";
+        let prevHeading = "";
+        const temp = [];
+        let idx = -1;
+        const setHeading = (pattern, data) => {
+            let hyphen = "-";
+            switch (pattern) {
+                case "number":
+                    hyphen = data.substring(0, data.indexOf('.'));
+                    data = data.replace(numberingPattern, '');
+                    break;
+                case "step":
+                    const stepNumbers = data.heading.match(/\bStep\s+(\d+)\b/g);
+                    const numbersOnly = stepNumbers.map(step => parseInt(step.match(/\d+/)[0]));
+                    hyphen = numbersOnly;
+                    data = data.replace(stepPattern, '');
+                    break;
+                case "letter":
+                    hyphen =  `${data.substring(0, data.indexOf('.'))}.`;
+                    data = data.replace(letterPattern, '');
+                    break;
+                default:
+                    data = data.replace(dashPattern, '');
+                    break;
+            }
+            if((heading.length <= 0 && !indent) || (heading === pattern && !indent) || (prevHeading !== pattern && indent) || (prevHeading === "empty")){
+                indent = false;
+                heading = pattern;
+                temp.push({
+                    hyphen,
+                    heading: data,
+                    item: []
+                });
+                ++idx;
+            }else if(heading !== pattern){
+                indent = true;
+                temp[idx].item.push({hyphen, data});
+            }
+            prevHeading = pattern;
+        };
         props.list.forEach((value, index, arr) => {
             arr[index] = value.trim();
+            const data = arr[index];
+            if(data.length > 0){
+                if(numberingPattern.test(data)){
+                    setHeading("number", data);
+                }else if(stepPattern.test(data)){
+                    setHeading("step", data);
+                }else if(letterPattern.test(data)){
+                    setHeading("letter", data);
+                }else if(dashPattern.test(data)){
+                    setHeading("dash", data);
+                }else{
+                    setHeading("empty", data);
+                }
+            }
         });
+        setList(temp);
     }, [props.list]);
 
     const expandMinimisedBtn = _ => {
@@ -33,73 +90,37 @@ export const ListItem = (props) => {
                 </button>
             </div>
             { itemClassName !== "minimised" && 
-            props.list.map((data, index) => (
-                (data.length > 0) && (
-                    numberingPattern.test(data) ? (
-                        numberingItem(index, isEditSelected, setSelectedStep, data)
-                    ) : (
-                        stepPattern.test(data) ? (
-                            stepItem(index, isEditSelected, setSelectedStep, data)
-                        ) : (
-                            letterPattern.test(data) ? (
-                                letterItem(index, isEditSelected, setSelectedStep, data)
-                            ) : (
-                                dashItem(index, isEditSelected, setSelectedStep, data)
-                            )
-                        )
-                    )
-                )
+            list.map((data, index) => (
+                mainItem(index, isEditSelected, setSelectedStep, data)
             ))}
             {(props.editStatus && itemClassName !== "minimised") && addItem()}
         </div>
     );
 };
 
-const numberingItem = (index, isEditSelected, setSelectedStep, data) => {
+const mainItem = (index, isEditSelected, setSelectedStep, data) => {
     return(
         <div key={index} className={`list-table4-list-item${isEditSelected(index) ? " active" : ""}`}>
-            <div style={{display: "flex", marginBottom: isEditSelected(index) ? "15px" : "0"}} onClick={() => setSelectedStep(index)}>
-                <h3>{data.substring(0, data.indexOf('.'))}</h3>
-                <text>{data.replace(numberingPattern, '')}</text>
+            <div style={{marginBottom: isEditSelected(index) ? "15px" : "0"}} onClick={() => setSelectedStep(index)}>
+                <div style={{display: "flex"}}>
+                    <h3>{data.hyphen}</h3>
+                    <text>{data.heading}</text>
+                </div>
+                {
+                    data.item.map((item) => (
+                        <div className='sub-items'>
+                            <h3>{item.hyphen}</h3>
+                            <text>{item.data}</text>
+                        </div>
+                    ))
+                }
             </div>
             {isEditSelected(index) && <EditPrompt/>}
         </div>
     );
 };
-const dashItem = (index, isEditSelected, setSelectedStep, data) => {
-    return(
-        <div key={index} className={`list-table4-list-item${isEditSelected(index) ? " active" : ""}`}>
-            <div style={{display: "flex", marginBottom: isEditSelected(index) ? "15px" : "0"}} onClick={() => setSelectedStep(index)}>
-                <h3>-</h3>
-                <text>{data.replace(dashPattern, '')}</text>
-            </div>
-            {isEditSelected(index) && <EditPrompt/>}
-        </div>
-    );
-};
-const letterItem = (index, isEditSelected, setSelectedStep, data) => {
-    return(
-        <div key={index} className={`list-table4-list-item${isEditSelected(index) ? " active" : ""}`}>
-            <div style={{display: "flex", marginBottom: isEditSelected(index) ? "15px" : "0"}} onClick={() => setSelectedStep(index)}>
-                <h3>{data.substring(0, data.indexOf('.'))}</h3>
-                <text>{data.replace(letterPattern, '')}</text>
-            </div>
-            {isEditSelected(index) && <EditPrompt/>}
-        </div>
-    );
-};
-const stepItem = (index, isEditSelected, setSelectedStep, data) => {
-    const stepNumbers = data.match(/\bStep\s+(\d+)\b/g);
-    const numbersOnly = stepNumbers.map(step => parseInt(step.match(/\d+/)[0]));
-    return(
-        <div key={index} className={`list-table4-list-item${isEditSelected(index) ? " active" : ""}`}>
-            <div style={{display: "flex", marginBottom: isEditSelected(index) ? "15px" : "0"}} onClick={() => setSelectedStep(index)}>
-                <h3>{numbersOnly}</h3>
-                <text>{data.replace(stepPattern, '')}</text>
-            </div>
-            {isEditSelected(index) && <EditPrompt/>}
-        </div>
-    );
+const subItems = () => {
+
 };
 
 const addItem = _ => {
