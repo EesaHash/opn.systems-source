d6be22365ad29ec43a3780e53c1e91cf3c65a53d
jonathanertanto <jonathanertanto@gmail.com>
2023-07-24 14:07:19 +1000

    sop frontend

--- front-end/src/table/components/ExpandMinimisedItem/ExpandMinimisedTableItem2.js
+++ front-end/src/table/components/ExpandMinimisedItem/ExpandMinimisedTableItem2.js
@@ -17,7 +17,7 @@ export const ExpandMinimisedTableItem2 = (props) => {
     return(
         <div key={props.index} className={`list-table4-list-item${props.isEditSelected(props.index) ? " active" : ""}`} onClick={() => itemClassName === "minimised" && expandMinimisedBtn()}>
             <h3>{props.data.hyphen}</h3>
-            <div style={{width:"100%", display: "grid"}}>
+            <div className='list-table4-list-item-content'>
                 <div className={`${props.isEditSelected(props.index) ? "edit-selected" : ""}`} style={{marginBottom: props.isEditSelected(props.index) ? "15px" : "0"}} onClick={() => props.setSelectedStep(props.index)}>
                     <div style={{display: "flex"}} className={props.data.item.length > 0 ? "heading" : ""} onClick={expandMinimisedBtn} >
                         <text>{props.data.heading}</text>
