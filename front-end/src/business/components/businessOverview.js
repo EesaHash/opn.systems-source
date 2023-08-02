5449ebe4739db85f733920eea846c84e68e7bf0d
jonathanertanto <jonathanertanto@gmail.com>
2023-08-02 16:56:15 +1000

    remove unused useeffect from business overview

--- front-end/src/business/components/businessOverview.js
+++ front-end/src/business/components/businessOverview.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 //import "../style/business.css";
 import "../style/businessOverview.css"
 import PeopleIcon from '../svg/peopleIcon';
