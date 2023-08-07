9264e8fef1a7ac237c59fe96e7170ea973870308
Eesa Hash <eesa6000@gmail.com>
2023-08-07 12:17:21 +1000

    added some comments

--- front-end/src/cj_policies/components/PoliciesDashboard.js
+++ front-end/src/cj_policies/components/PoliciesDashboard.js
@@ -3,29 +3,48 @@ import "../style/policies.css";
 import { FolderList } from '../../table/components/Folder/FolderList';
 import { openAccessLimitForm, openFutureFeatureWarningForm } from '../../dashboard/page/dashboard_main';
 
+/**
+ * PoliciesDashboard functional component.
+ * This component renders a dashboard for managing policies.
+ *
+ * @param {Object} props - The props object for the component.
+ * @param {Array} props.journeys - An array containing the list of journeys.
+ * @param {Array} props.policies - An array containing the list of policies.
+ * @param {string} props.activeLink2 - The active link for the secondary table (unused in this implementation).
+ * @returns {JSX.Element} - The PoliciesDashboard component's UI.
+ */
 export const PoliciesDashboard = (props) => {
     const [setJourney] = useState({});
     const [setIndex] = useState(-1);
+
+    // useEffect hook to handle display of main and secondary tables.
     useEffect(() => {
         const mainTable = document.getElementById("policies-main-table");
         const secondaryTable = document.getElementById("policies-secondary-table");
-        if(mainTable && secondaryTable){
+
+        if (mainTable && secondaryTable) {
             mainTable.style.display = "block";
             setJourney({});
             secondaryTable.style.display = "none";
         }
         // eslint-disable-next-line
     }, [props.activeLink2]);
+
+    // Function to open the form for creating a new journey.
     const openCreateJourneyForm = _ => {
-        if(props.journeys.length > 0)
+        if (props.journeys.length > 0)
             return openAccessLimitForm();
         document.getElementById("createClientJourney").style.display = "block";
     };
+
+    // Function to open the policy list for a specific journey.
     const openPilicyList = (param, index) => {
         const mainTable = document.getElementById("policies-main-table");
         const secondaryTable = document.getElementById("policies-secondary-table");
+
         openFutureFeatureWarningForm();
-        if(mainTable && secondaryTable){
+
+        if (mainTable && secondaryTable) {
             mainTable.style.display = "none";
             setJourney(param);
             setIndex(index);
@@ -33,15 +52,28 @@ export const PoliciesDashboard = (props) => {
         }
     };
 
-    return(
+    return (
+        // Main container for the policies dashboard.
         <div className='policies'>
+            {/* 
+                FolderList component renders a table with folders (journeys) and their associated policies.
+                
+                @param {string} id - ID for the main table element.
+                @param {string} title - Title displayed above the table.
+                @param {Array} list - An array containing the list of journeys.
+                @param {Array} list2 - An array containing the list of policies, or an empty array if no policies exist.
+                @param {function} addNewBtn - Function to be called when the "Add New" button is clicked. 
+                                             It opens a form for creating a new journey or shows access limit form.
+                @param {function} itemActionBtn - Function to be called when an item (journey) is clicked. 
+                                                  It opens the policy list for the clicked journey (not fully implemented here).
+            */}
             <FolderList 
-                id = "policies-main-table"
-                title = "Policies"
-                list = {props.journeys}
-                list2 = {props.policies.length > 0 ? props.policies : [[]]}
-                addNewBtn = {openCreateJourneyForm}
-                itemActionBtn = {openPilicyList}
+                id="policies-main-table"
+                title="Policies"
+                list={props.journeys}
+                list2={props.policies.length > 0 ? props.policies : [[]]}
+                addNewBtn={openCreateJourneyForm}
+                itemActionBtn={openPilicyList}
             />
         </div>
     );
--- front-end/src/cj_procedure/components/CreateProcedure.js
+++ front-end/src/cj_procedure/components/CreateProcedure.js
@@ -2,51 +2,69 @@ import React, { useState } from 'react';
 import "../style/procedure.css";
 import { loadingPage } from '../../warning_pages/components/loadingPage';
 
+/**
+ * CreateProcedure functional component.
+ * This component renders a form to create a new procedure.
+ *
+ * @param {Object} props - The props object for the component (unused in this implementation).
+ * @returns {JSX.Element} - The CreateProcedure component's UI.
+ */
 export const CreateProcedure = (props) => {
     const [loading, setLoading] = useState(false);
     const titlePage = "Create Procedure";
+
+    // Function to close the procedure form and reset input fields.
     const closeForm = _ => {
         document.getElementById("procedure-title").value = "";
         document.getElementById("create-procedure-step1").style.display = "block";
         setLoading(false);
         document.getElementById("createProcedureForm").style.display = "none";
     };
+
+    // Function to generate the procedure file.
     const generate = _ => {
         const title = document.getElementById("procedure-title").value;
-        if(!title)
+        if (!title)
             return alert("Please fill in all fields!");
+
         document.getElementById("create-procedure-journey-step1").style.display = "none";
         setLoading(true);
-        
     };
+
+    // Function to handle "Enter" keypress event.
     const handleKeypress = e => {
-        if(e.key === "Enter"){
+        if (e.key === "Enter") {
             generate();
         }
     };
+
+    // Function to render step 1 of the procedure creation form.
     const step1 = _ => {
-        return(
+        return (
             <div id="create-procedure-step1" className="content-form">
                 <h2>{titlePage}</h2>
-                <hr/>
+                <hr />
                 <div className='pop-up-input'>
                     <label>Procedure Title</label>
-                    <input 
-                        id = "procedure-title"
+                    <input
+                        id="procedure-title"
                         type='text'
-                        onKeyPress={handleKeypress} 
+                        onKeyPress={handleKeypress}
                     />
                 </div>
                 <div className='pop-up-button'>
                     <button className='cancel-button' onClick={closeForm}>Cancel</button>
-                    <button className='next-button' onClick={generate} >Generate File</button>
+                    <button className='next-button' onClick={generate}>Generate File</button>
                 </div>
             </div>
         );
     };
-    return(
+
+    return (
         <section id="createProcedureForm" className="form-popup center form-container create-form">
+            {/* Render step 1 of the procedure creation form */}
             {step1()}
+            {/* Render loadingPage component when loading is true */}
             {loading && loadingPage(titlePage, "AI is writing procedures for", document.getElementById("procedure-title").value)}
         </section>
     );
